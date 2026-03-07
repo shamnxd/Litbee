8fd668361b14c2749e88bae72fdd0e3e358738fc
Shamand <shamnadthayyil8@gmail.com>
2026-03-07 15:38:01 +0530

    feat: core server setup and entry point

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UrlsModule } from './urls/urls.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    AuthModule,
+    UsersModule,
+    UrlsModule,
+  ],
+
   controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    credentials: true,
+  });
+
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => console.log('App is running...'))
+  .catch(() => console.log('App is fialed to start'));
